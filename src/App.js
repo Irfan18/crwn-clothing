b10d8cc739be76f15a729745ba1cff583d1707b8
irfan <irfan.shaikh@adapty.com>
2020-08-13 20:50:05 +0530

    added firebase utils and implemented user store in database

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ShopPage from './pages/shop/shop';
 import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up';
 import Header from './components/header/header';
 
-import { auth } from './firebase/firebase.utils';
+import { createUserProfileDocument, auth } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -20,10 +20,28 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({
-        currentUser: user
-      });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      console.log('Hi yes I am in authChange')
+      if(userAuth) {
+        console.log('Checked userauth and creating user profile')
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapshot => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data()
+            }
+          })
+
+          console.log(this.state, 'check state in snap')
+        })
+      } else {
+        this.setState({
+          currentUser: userAuth
+        })
+      }
+      
     })
   }
 
